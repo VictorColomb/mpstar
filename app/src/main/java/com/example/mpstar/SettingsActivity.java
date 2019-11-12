4e63663a6a29c9ae5481b9195f3618be735b5ad7
Victor <vic.col@hotmail.fr>
2019-11-12 23:55:27 +0100

    name list in preferences generated from spreadsheet data

--- app/src/main/java/com/example/mpstar/SettingsActivity.java
+++ app/src/main/java/com/example/mpstar/SettingsActivity.java
@@ -5,7 +5,9 @@ import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.ArraySet;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TimePicker;
@@ -20,11 +22,18 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
+import com.example.mpstar.model.Student;
+import com.example.mpstar.save.FilesIO;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +47,18 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
-        //Search for current settings and en/disable individual settings
     }
 
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        FilesIO filesIO;
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SharedPreferences pref = this.getActivity().getSharedPreferences("myPreferences", 0);
+            final SharedPreferences.Editor pref_editor = pref.edit();
 
             //disable individual notification settings if global notifications are disabled
             SwitchPreference preference_notifications_activated = findPreference("notifications_activated");
@@ -62,6 +74,35 @@ public class SettingsActivity extends AppCompatActivity {
                                 preference_notifications_birthdays.setChecked(false);
                             if (preference_notifications_colles != null)
                                 preference_notifications_colles.setChecked(false);
+                        }
+                        return true;
+                    }
+                });
+            }
+
+            //name from and to sharedPreferences
+            filesIO = new FilesIO(getContext());
+            List names = filesIO.readNamesList();
+            CharSequence[] namesCs = new CharSequence[names.size()];
+            int index = 0;
+            for (Object name : names) {
+                namesCs[index++] = name.toString();
+            }
+
+            final ListPreference preference_perso_name = findPreference("perso_name");
+            if (preference_perso_name != null) {
+                preference_perso_name.setEntries(namesCs);
+                preference_perso_name.setEntryValues(namesCs);
+                //preference_perso_name.setValue(pref.getString("perso_name", null));
+
+                //FUCKING PROBLEM... sharedPreference is set to the preference's value before the change...........
+                preference_perso_name.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        if (preference_perso_name.getValue() != "") {
+                            pref_editor.putString("perso_name", preference_perso_name.getValue());
+                            pref_editor.apply();
+                            Log.i("mpstar_preferences", "SharedPreference perso_name set to : "+preference_perso_name.getValue());
                         }
                         return true;
                     }
