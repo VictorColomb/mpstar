b3536b2b40ecf1b0e7a27ba5636cc8f2624ab67b
Jack Royer <jack.royer.23@gmail.com>
2019-11-13 19:59:36 +0100

    Colors seat

--- app/src/main/java/com/example/mpstar/MainActivity.kt
+++ app/src/main/java/com/example/mpstar/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.InetAddresses
 import android.net.NetworkCapabilities
@@ -58,9 +59,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun showClassPlan(){
         val r = resources
+        val preferences = getSharedPreferences("mySharedPreferences", 0)
+        val my_name = preferences.getString("perso_name", "JEFF")
         for (student in students){
             val textView: TextView = findViewById(r.getIdentifier("seat" + student.myRow.toString() + student.myColumn.toString(), "id", packageName))
             textView.text = student.myName
+            if(student.myName == my_name){
+                textView.setBackgroundColor(Color.parseColor("#3f51b5"))
+                textView.setTextColor(Color.parseColor("#ffffff"))
+            }
         }
     }
 
